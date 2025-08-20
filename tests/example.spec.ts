a01d182d3338890b037efc2351a06c583101ee42
PramodTestHub <pramodhmh26@gamil.com>
2025-08-20 20:24:06 +0530

    modify file

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
-  await page.goto('https://www.bigbasket.com/?utm_source=google&utm_medium=cpc&utm_campaign=Brand-PAN-Jan25&gad_source=1&gclid=Cj0KCQiAz6q-BhCfARIsAOezPxntZrk9Q7ISNy1cQIWDUkIKim1ThgxfOcg0erIzqZizjZTrnqsQf9saAjWoEALw_wcB');
+  await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
